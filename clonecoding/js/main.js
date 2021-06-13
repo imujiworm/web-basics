ccb2199b2c0ab9355dddb8fe7b563b034c6d38ad
imujiworm <80830606+imujiworm@users.noreply.github.com>
2021-06-13 23:40:16 +0900

    Add files via upload

---
+++ clonecoding/js/main.js
@@ -0,0 +1,18 @@
+// 햄버거 바 아이콘 선택자
+const openMenuEl = document.getElementById("btn--open")
+// 닫는 아이콘 선택자
+const closeMenuEl = document.getElementById("btn--close")
+// 사이드 메뉴 선택자
+const navMenuEl = document.getElementById("nav_menu")
+
+openMenuEl.addEventListener("click", function(){
+    openMenuEl.style.display = 'none'
+    closeMenuEl.style.display = 'block'
+    navMenuEl.style.transform = 'translate(50%)'
+}) 
+
+closeMenuEl.addEventListener("click", function(){
+    closeMenuEl.style.display = 'none'
+    openMenuEl.style.display = 'block'
+    navMenuEl.style.transform = 'translate(-120%)'
+}) 
